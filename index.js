11932b9ceb6bf0b36b1b5f2143d49b1ece61a454
Om Shivhare <omshivhare666@gmail.com>
2026-05-25 13:34:00 +0530

    Added edit functionality

--- index.js
+++ index.js
@@ -31,10 +31,20 @@ app.get("/file/:filename" , (req, res) => {
     })
 })
 
+app.get("/edit/:filename" , (req, res) => {
+    res.render('edit', {filename: req.params.filename});
+})
+
 app.post("/create" , (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,() => {
         res.redirect("/");
     })
+})
+
+app.post("/edit" , (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}` ,function(err) {
+        res.redirect('/');
+    });
 })
 
 
